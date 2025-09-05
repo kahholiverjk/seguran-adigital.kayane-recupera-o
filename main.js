415fe8d4af6a667352fdaa300db6830f68dc7312
kahh_oliverjk <140529420+kahholiverjk@users.noreply.github.com>
2025-09-05 14:22:20 +0000

    ..

--- main.js
+++ main.js
@@ -144,4 +144,3 @@ function classificaSenha(tamanhoAlfabeto) {
     valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2 ** entropia / (100e6 * 60 * 60 * 24)) + " dias para descobrir essa senha.";
 }
 
-
